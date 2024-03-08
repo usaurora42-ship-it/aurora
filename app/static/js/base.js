1411d8e885d373ef1bcada7a52ec6493c27026ae
prirodovalho <prirodovalho@gmail.com>
2024-03-08 16:55:00 -0300

    alteracao client

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -12,4 +12,28 @@ dropdown.onchange = function(event){
     if(dropdown.value=="pj"){
         $('input[type="document"]').mask('00.000.000/0000-00')
     }
+};
+
+function msgcheckFunction(){
+    var checkBox = document.getElementById("msgcheck");
+
+    var text = document.getElementById("text");
+
+    if (checkBox.checked == true){
+        document.getElementById("gifted_data").style.visibility = "visible";
+        document.getElementById("gifted_name").style.visibility = "visible";
+        document.getElementById("gifted_phone").style.visibility = "visible";
+        document.getElementById("gifted_ocasion").style.visibility = "visible";
+        document.getElementById("gifted_message").style.visibility = "visible";
+        document.getElementById("signature_card").style.visibility = "visible";
+        document.getElementById("gifted_name").required = true;
+    } else {
+        document.getElementById("gifted_data").style.visibility = "hidden";
+        document.getElementById("gifted_name").style.visibility = "hidden";
+        document.getElementById("gifted_phone").style.visibility = "hidden";
+        document.getElementById("gifted_ocasion").style.visibility = "hidden";
+        document.getElementById("gifted_message").style.visibility = "hidden";
+        document.getElementById("signature_card").style.visibility = "hidden";
+        document.getElementById("gifted_name").required = false;
+    }
 }
