02b5242b18fb91bd7084cb7d45224b4317292511
prirodovalho <prirodovalho@gmail.com>
2024-06-08 13:48:11 -0300

    cadastro de produtos

--- app/blueprints/site/products/products.py
+++ app/blueprints/site/products/products.py
@@ -1,13 +1,12 @@
 # encoding: utf-8
 import re
 import os
-from flask import Flask, flash, render_template, make_response, request, redirect, url_for, send_from_directory
+from flask import Flask, flash, render_template, make_response, request, redirect, url_for
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 from app.blueprints.site import SiteBlueprint
 from werkzeug.utils import secure_filename
-from flask_uploads import UploadSet, IMAGES, configure_uploads
 
 from app import logging
 from app import environment
@@ -18,25 +17,6 @@ from app.model.enum import StatusEnum
 
 
 LOGGER = logging.getLogger(__name__)
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'asldfkjlj'
-app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
-
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
-
-class UploadForm(FlaskForm):
-	photo = FileField(
-		validators=[
-			FileAllowed(photos, 'Somente imagens são permitidas'),
-			FileRequired('O campo não pode estar vazio')
-        ]
-    )
-	submit = SubmitField('Upload')
-     
-@app.route('/images/products/<filename>')
-def get_file(filename):
-	return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
 
 @SiteBlueprint.route('/products/products')
 def products_get():
@@ -78,15 +58,6 @@ def products_get():
     return resp 
 
 @SiteBlueprint.route('/products/products', methods=['GET', 'POST'])	
-def upload_image():
-	form = UploadForm()
-	if form.validate_on_submit():
-		filename = photos.save(form.photo.data)
-		file_url = url_for('get_file', filename=filename)
-	else:
-		file_url = None
-	return render_template('products/products.html', form=form, file_url=file_url)
-
 def products_post():      
 
     data = request.form.to_dict() or {}
@@ -142,9 +113,7 @@ def products_post():
                 resp = make_response(render_template('products/products.html',
                             success=False,
                             errors=model_product.errors,
-                            data_input=data,
-                            form=form, 
-                            file_url=file_url))      
+                            data_input=data))      
                 resp.mimetype = 'text/html'
                 return resp 
             
