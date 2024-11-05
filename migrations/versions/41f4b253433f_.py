accc9fb90797cae8e93e6551dfcc3625cf2499c6
Lucas Mateus <luma@soniasoft.com>
2024-11-05 00:39:31 -0300

    fix migration

---
+++ migrations/versions/41f4b253433f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 41f4b253433f
+Revises: 0c120662f2ca, 1918f37d6fb3
+Create Date: 2024-11-05 00:36:41.277561
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '41f4b253433f'
+down_revision = ('0c120662f2ca', '1918f37d6fb3')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
